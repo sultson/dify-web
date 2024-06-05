d81faf93f70d45db906895d343a7e035b3c8bf04
sultson <11300079+sultson@users.noreply.github.com>
2024-06-05 09:57:32 +0200

    fix: typo

--- config/index.ts
+++ config/index.ts
@@ -6,7 +6,7 @@ export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 't
 export const APP_INFO: AppInfo = {
   title: 'Productbeschrijving Maker',
   description: 'Agentics x OutdoorXL',
-  copyright: 'Agentix',
+  copyright: 'Agentics',
   privacy_policy: '',
   default_language: 'en',
 }
