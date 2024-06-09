d3907755dd1c5bfae7c3b8656adb72a0e4700b82
sultson <11300079+sultson@users.noreply.github.com>
2024-06-09 22:10:24 +0200

    chore: env structure

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,14 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+export const CLIENT = `${process.env.NEXT_PUBLIC_CLIENT}`
+export const APP_ID = `${process.env[`NEXT_PUBLIC_APP_${CLIENT}_ID`]}`
+export const API_KEY = `${process.env[`NEXT_PUBLIC_APP_${CLIENT}_KEY`]}`
+
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Productbeschrijving Maker',
-  description: 'Agentics x OutdoorXL',
+  title: `${CLIENT === 'PMO' ? 'Content' : 'Productbeschrijving'} Generator`,
+  description: `Agentics x ${CLIENT === 'PMO' ? 'PMO' : 'OutdoorXL'}`,
   copyright: 'Agentics',
   privacy_policy: '',
   default_language: 'en',
